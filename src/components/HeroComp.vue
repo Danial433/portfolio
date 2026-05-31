37e0f365e0e8e0e90406dc69c1b72d1b094add03
Danial <danialaqil433@gmail.com>
2026-05-31 16:21:23 +0200

    Update profile image

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -3,7 +3,7 @@
     <div class="flex max-[412px]:flex-col gap-4 max-[412px]:items-center">
       <div class="flex-shrink-0 mt-2">
         <img
-          src="/kyuiki.png"
+          src="/danial.jpg"
           v-motion
           :initial="{
             rotate: -10,
