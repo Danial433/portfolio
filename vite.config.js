608e46b5a9539787d004c0774231ecfbb0d2d3a4
Danial <danialaqil433@gmail.com>
2026-05-31 15:39:56 +0200

    Fix github Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
-// Nothing Really changed here - Kyuiki
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
